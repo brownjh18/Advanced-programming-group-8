f6b8e61d24e378f44dc47f047aceeaa657feca94
Gachiza <gachiza17@gmail.com>
2025-08-29 14:28:15 +0200

    project set up

--- projects/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Project(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.title
-
